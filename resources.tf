f52efe7356cad151c6ba772f88dd65a4f1c4129b
Ian hunt <ihuntenator@gmail.com>
2021-10-07 08:32:46 +1300

    Tidy vars

--- resources.tf
+++ resources.tf
@@ -5,31 +5,31 @@ resource "azurerm_resource_group" "failover" {
 }
 
 resource "azurerm_postgresql_server" "failover" {
-  name                = "failover-psqlserver"
+  name                = var.azurerm_postgresql_server_name
   location            = azurerm_resource_group.failover.location
   resource_group_name = azurerm_resource_group.failover.name
 
   administrator_login          = var.db_login
   administrator_login_password = var.db_password
 
-  sku_name   = var.default_sku
+  sku_name   = var.sku_name
   version    = var.postgress_version
-  storage_mb = 5120
+  storage_mb = var.storage_mb
 
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-  auto_grow_enabled            = false
+  backup_retention_days        = var.backup_retention
+  geo_redundant_backup_enabled = var.geo_redundant_backup
+  auto_grow_enabled            = var.auto_grow
 
-  ssl_enforcement_enabled          = true
-  ssl_minimal_tls_version_enforced = "TLS1_2"
+  ssl_enforcement_enabled          = var.ssl_enforcement
+  ssl_minimal_tls_version_enforced = var.ssl_minimal_tls_version
 
   tags = merge(var.default_tags)
 }
 
 resource "azurerm_postgresql_database" "failover" {
-  name                = "failoverdb"
+  name                = var.azurerm_postgresql_database
   resource_group_name = azurerm_resource_group.failover.name
   server_name         = azurerm_postgresql_server.failover.name
-  charset             = "UTF8"
-  collation           = "English_United States.1252"
+  charset             = var.charset
+  collation           = var.collation
 }
--- variables.tf
+++ variables.tf
@@ -13,6 +13,12 @@ variable "default_tags" {
   }
 }
 
+variable "azurerm_postgresql_server_name" {
+  type        = string
+  description = "Postgress server name"
+  default     = "failover-psqlserver"
+}
+
 variable "db_login" {
   type        = string
   description = "Postgress administrator login"
@@ -24,7 +30,7 @@ variable "db_password" {
   description = "Postgress administrator password"
 }
 
-variable "default_sku" {
+variable "sku_name" {
   type        = string
   description = "SKU name"
   default     = "B_Gen5_1"
@@ -34,4 +40,64 @@ variable "postgress_version" {
   type        = number
   description = "Postgres version"
   default     = 11
+}
+
+variable "storage_mb" {
+  type        = number
+  description = "Minimum storage"
+  default     = 5120
+}
+
+variable "backup_retention" {
+  type        = number
+  description = "Days to retain backup"
+  default     = 7
+}
+
+variable "geo_redundant_backup" {
+  type        = bool
+  description = "GEO redyndant backup"
+  default     = false
+}
+
+variable "azurerm_postgresql_database" {
+  type        = string
+  description = "Postgres database name"
+  default     = "failover-db"
+}
+
+variable "auto_grow" {
+  type        = bool
+  description = "Enable auto grow of storage"
+  default     = false
+}
+
+variable "ssl_enforcement" {
+  type        = bool
+  description = "Enforce TLS"
+  default     = true
+}
+
+variable "ssl_minimal_tls_version" {
+  type        = string
+  description = "Minimal TLS version"
+  default     = "TLS1_2"
+}
+
+variable "azurerm_postgresql_database_name" {
+  type        = string
+  description = "Postgres database name"
+  default     = "failoverdb"
+}
+
+variable "charset" {
+  type        = string
+  description = "Postgres character sset"
+  default     = "UTF8"
+}
+
+variable "collation" {
+  type        = string
+  description = "Postgres collation"
+  default     = "English_United States.1252"
 }
