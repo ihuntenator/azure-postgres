9a2c13412501614d19c407f0682a56b572df40f2
Ian hunt <ihuntenator@gmail.com>
2021-10-27 09:01:02 +1300

    more config

--- provider.tf
+++ provider.tf
@@ -8,3 +8,13 @@ terraform {
 
   required_version = ">= 1.0.0"
 }
+
+provider "azurerm" {
+  features {}
+}
+
+#resource "azurerm_resource_group" "rg" {
+#  name     = "myTFResourceGroup"
+#  location = "westus2"
+#}
+
